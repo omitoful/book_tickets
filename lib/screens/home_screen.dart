7bf109a6cb08c6ccdcda79bbc3eecd10574f2578
Kevin <kevin@twjoin.com>
2022-10-14 16:57:07 +0800

    setup utils & home header

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,65 @@
+import 'package:booktickets/utils/app_styles.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        children: [
+          Container(
+            /// padding: EdgeInsets.only(left: 20, right: 20) -> same as down below
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                const Gap(40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Good morning", style: Styles.headLineStyle3),
+                        const Gap(5),
+                        Text("Book Tickets", style: Styles.headLineStyle1),
+                      ],
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: const DecorationImage(
+                            fit: BoxFit.fitHeight,
+                            image: AssetImage("assets/img_1.png")
+                        )
+                      ),
+                    )
+                  ],
+                ),
+                const Gap(25),
+                Row(
+                  children: [
+                    const Icon(FluentSystemIcons.ic_fluent_search_regular, color: Color(0xFFBFC205)),
+                    Text("Search", style: Styles.headLineStyle4)
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
