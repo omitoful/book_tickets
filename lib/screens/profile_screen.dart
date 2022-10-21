42d49fa12d4a8406bd46224805136b5ecada48d3
Kevin <kevin@twjoin.com>
2022-10-21 13:39:13 +0800

    setup profile view header

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,96 @@
+import 'package:booktickets/utils/app_layout.dart';
+import 'package:booktickets/utils/app_styles.dart';
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: AppLayout.getHeight(20), vertical: AppLayout.getHeight(20)),
+        children: [
+          Gap(AppLayout.getHeight(40)),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: AppLayout.getHeight(86),
+                width: AppLayout.getHeight(86),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(AppLayout.getHeight(10)),
+                  image: const DecorationImage(
+                    image: AssetImage("assets/img_1.png")
+                  )
+                ),
+              ),
+              Gap(AppLayout.getHeight(10)),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text("Book Tickets", style: Styles.headLineStyle1,),
+                  Gap(AppLayout.getHeight(2)),
+                  Text("New-York", style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.grey.shade500)
+                  ),
+                  Gap(AppLayout.getHeight(8)),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(AppLayout.getHeight(100)),
+                      color: const Color(0xFFFEF4F3),
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: AppLayout.getHeight(3), vertical: AppLayout.getHeight(3)),
+                    child: Row(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(3),
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Color(0xFF526799),
+                          ),
+                          child: const Icon(
+                            FluentSystemIcons.ic_fluent_shield_filled,
+                            color: Colors.white,
+                            size: 15,
+                          ),
+                        ),
+                        Gap(AppLayout.getHeight(5)),
+                        const Text("Premium status", style: TextStyle(
+                          color: Color(0xFF526799),
+                          fontWeight: FontWeight.w600
+                        )),
+                        Gap(AppLayout.getHeight(5)),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              const Spacer(),
+              Column(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      print("Edit tapped.");
+                    },
+                    child: Text(
+                      "Edit",
+                      style: Styles.textStyle.copyWith(color: Styles.primaryColor, fontWeight: FontWeight.w300)
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+          
+        ],
+      ),
+    );
+  }
+}
